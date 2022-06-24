0fd2d782fc4b6f500437eb53a95aaa0475163ad4
TylerDonghwi <tylerdonghwikim@email.com>
2022-06-24 15:22:06 +1200

    css for snake is done in a way

--- snake/snake.js
+++ snake/snake.js
@@ -1,1 +1,8 @@
-"use strict";
+const audio = document.querySelector("#audio-loss")
+const button = document.querySelector('#button')
+
+// audio plays when you lose
+button.addEventListener("click", () => {
+    audio.currentTime = 0
+    audio.play()
+})
