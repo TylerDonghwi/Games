b0ba825f8ae5c621001d0330b43c14e4f5342463
TylerDonghwi <tylerdonghwikim@email.com>
2022-06-24 17:07:30 +1200

    win counts implemented

--- RPS/rps.js
+++ RPS/rps.js
@@ -2,10 +2,16 @@ const playerText = document.querySelector("#playerText")
 const computerText = document.querySelector("#computerText")
 const resultText = document.querySelector("#result")
 const choiceBtns = document.querySelectorAll(".choiceBtn")
+const resetBtn = document.querySelector("#resetBtn")
 
 let player
 let computer
 let result
+
+let playerWinCounts = 0;
+let computerWinCounts = 0;
+const playerWins = document.querySelector("#playerWins")
+const computerWins = document.querySelector('#computerWins')
 
 choiceBtns.forEach(button => {
     button.addEventListener('click', () => {
@@ -14,7 +20,15 @@ choiceBtns.forEach(button => {
         playerText.textContent = `Player: ${player}`
         computerText.textContent = `Computer: ${computer}`
         resultText.textContent = `Result: ${checkWinner()}`
+        updateWinCounts();
     })
+})
+
+resetBtn.addEventListener('click', () => {
+    playerWinCounts = 0
+    computerWinCounts = 0
+    playerWins.textContent = `Player: ${playerWinCounts}`
+    computerWins.textContent = `Computer: ${computerWinCounts}`
 })
 
 function computerTurn() {
@@ -40,4 +54,14 @@ function checkWinner() {
     } else if (computer == "SCISSORS") {
         return (player == "ROCK") ? "Player Wins!" : "Computer Wins!"
     }
+}
+
+function updateWinCounts() {
+    if (resultText.textContent == "Result: Player Wins!") {
+        playerWinCounts++
+    } else if (resultText.textContent == "Result: Computer Wins!") {
+        computerWinCounts++
+    }
+    playerWins.textContent = `Player: ${playerWinCounts}`
+    computerWins.textContent = `Computer: ${computerWinCounts}`
 }
